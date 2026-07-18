170cb2b1a902cfe7948a39893763cc5f1cfba1dc
ShreyasKudahalli <shreyaskudahalli1857@gmail.com>
2026-07-19 00:23:15 +0530

    UI

--- Frontend/src/sections/Projects.jsx
+++ Frontend/src/sections/Projects.jsx
@@ -76,7 +76,7 @@ export const Projects = () => {
                  to-transparent opacity-60"
                 />
                 {/* Overlay Links */}
-                <div className="absolute inset-0 flex items-center justify-center gap-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <div className="absolute inset-0 flex items-center justify-center gap-4 opacity-100 md:opacity-0 md:group-hover:opacity-100 transition-opacity duration-300 ">
                   <a
                     href={project.link}
                     className="p-3 rounded-full glass hover:bg-primary hover:text-primary-foreground transition-all"
